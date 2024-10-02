8fad016e9c85c1e2676cbe312f5745ab6a79eed4
DESKTOP-52351T8\Admin <bf980326@gmail.com>
2024-10-02 16:59:50 +0900

    1. 플레이어 셋팅, 점프, 이동, 대기 2. 카메라 셋팅, 배경 및 이동 배경 구현

---
+++ Assets/MyBird/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace MyBird
+{
+    public class CameraController : MonoBehaviour
+    {
+        #region Variables
+        public Transform player;
+        [SerializeField] private float offset = 1.5f;
+        #endregion
+
+        private void LateUpdate()
+        {
+            FollowPlayer();
+        }
+
+        // 플레이어 따라가기
+        void FollowPlayer()
+        {
+            transform.position = new Vector3(player.position.x + offset, transform.position.y, transform.position.z);
+        }
+    }
+}
+
---
+++ Assets/MyBird/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyBird
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region Variables
+        public static bool IsStart { get; set; }
+        #endregion
+
+        private void Start()
+        {
+            // √ ±‚»≠
+            IsStart = false;
+        }
+    }
+}
+
